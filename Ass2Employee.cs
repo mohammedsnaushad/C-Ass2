66d47e73ae71863b0f535bb1885a9959c2eec7b0
mohammedsnaushad <102455221+mohammedsnaushad@users.noreply.github.com>
2022-05-06 11:36:17 +0530

    Update Ass2Employee.cs

--- Ass2Employee.cs
+++ Ass2Employee.cs
@@ -5,8 +5,7 @@
 	using System.Text;
 	using System.Threading.Tasks;
 
-	using System.ComponentModel.DataAnnotations;
-	using System.ComponentModel.DataAnnotations.Schema;
+	
 
 	namespace LibwareLib
 	{
